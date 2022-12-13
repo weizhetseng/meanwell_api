e261ab5d0d9f30dce1ca743b9dfc2d1a98486302
WeiZheTseng <weizhetseng@gmail.com>
2022-12-13 23:32:27 +0800

    night

--- src/views/ForgotPasswordView.vue
+++ src/views/ForgotPasswordView.vue
@@ -1,53 +1,62 @@
 <template>
-    <div class="page_main">
-        <main>
-            <div class="page_content">
-                <section class="loginTopIcon">
-                    <div class="loginBigIcon"><img src="../assets/img/LoginBigIcon.svg" alt=""></div>
-                </section>
-                <section class="loginContentBox">
-                    <div class="ContentBoxTitle">忘記密碼</div>
-                    <div class="signup_User_account">
-                        <input type="text" class="signup_User_accountInput" placeholder="請輸入6-20英數字元" required
-                            v-model="user.Email">
-                    </div>
-                    <div class="Boxbarmessage">請輸入註冊時使用的電子信箱，我們會將新密碼傳送至信箱，請登入立即修改密碼。</div>
-                    <div class="Boxbarbuttem2">
-                        <button class="pageButtem" @click="forgotPassword()">確認送出</button>
-
-                    </div>
-                </section>
-            </div>
-        </main>
-    </div>
+  <div class="page_main">
+    <main>
+      <div class="page_content">
+        <section class="loginTopIcon">
+          <div class="loginBigIcon">
+            <img src="../assets/img/LoginBigIcon.svg" alt="" />
+          </div>
+        </section>
+        <section class="loginContentBox">
+          <div class="ContentBoxTitle">忘記密碼</div>
+          <div class="signup_User_account">
+            <input
+              type="text"
+              class="signup_User_accountInput"
+              placeholder="請輸入6-20英數字元"
+              required
+              v-model="user.Email"
+            />
+          </div>
+          <div class="Boxbarmessage">
+            請輸入註冊時使用的電子信箱，我們會將新密碼傳送至信箱，請登入立即修改密碼。
+          </div>
+          <div class="Boxbarbuttem2">
+            <button class="pageButtem" @click="forgotPassword()">
+              確認送出
+            </button>
+          </div>
+        </section>
+      </div>
+    </main>
+  </div>
 </template>
 <script setup>
-import axios from 'axios';
-import { ref } from 'vue';
-import router from '../router';
+import axios from "axios";
+import { ref } from "vue";
+import router from "../router";
 
-
-
-const user = ref(
-    {
-        Email: '',
-    }
-)
+const user = ref({
+  Email: "",
+});
+//發送新密碼
 function forgotPassword() {
-    const api = `${import.meta.env.VITE_APP_API}API_App/MemberData/ForgetPassword`;
-    axios.post(api, {
-        "u_id": user.value.Email,
-        "Lang": "string"
-    }).then((res) => {
-        console.log(res)
-        if (res.data.success) {
-            alert('發送成功')
-            router.push('/login')
-        } else {
-            alert(res.data.message)
-        }
+  const api = `${
+    import.meta.env.VITE_APP_API
+  }API_App/MemberData/ForgetPassword`;
+  axios
+    .post(api, {
+      u_id: user.value.Email,
+      Lang: "string",
     })
+    .then((res) => {
+      console.log(res);
+      if (res.data.success) {
+        alert("發送成功");
+        router.push("/login");
+      } else {
+        alert(res.data.message);
+      }
+    });
 }
-
-
 </script>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,164 +1,204 @@
-
 <template>
-    <div class="page_main">
-        <main>
-            <div class="page_content">
-                <div class="indexPageTitle">多元服務，價值共享</div>
-                <section class="indexActivityBar">
-                    <div class="indAct_item_Box">
-                        <ul class="indAct_ul01">
-                            <li v-for="item in indexLink" :key="item.id">
-                                <router-link :to="`/Course/Meeting/${item.route}`">
-                                    <div class="atcImgBox"><img :src="`/src/assets/img/${item.imgUrl}`" alt=""></div>
-                                    <div class="atcText" :style="{ color: item.color }">{{ item.link }}</div>
-                                </router-link>
-                            </li>
-                        </ul>
-                        <ul class="indAct_ul02">
-                            <li>
-                                <router-link to="#">
-                                    <div class="atcImgBox"><img src="../assets/img/activity_4.svg" alt=""></div>
-                                    <div class="atcText CaringCare">關懷照護</div>
-                                </router-link>
-                            </li>
-                            <li>
-                                <router-link to="#">
-                                    <div class="atcImgBox"><img src="../assets/img/activity_5.svg" alt=""></div>
-                                    <div class="atcText GiftingPlatform">禮贈平台</div>
-                                </router-link>
-                            </li>
-                        </ul>
-                    </div>
-                </section>
-                <section class="AnnouncementSystem">
-                    <div class="AnnouncementSystemBox">
-                        <ul>
-                            <li>
-                                <div class="AnnTop">
-                                    <div class="AnnTopIcon"><img class="messageIcon"
-                                            src="../assets/img/message_icon.svg" alt=""></div>
-                                    <div class="AnnTopText">訊息通知</div>
-                                </div>
-                                <div class="AnnContent">
-                                    <ul v-if="store.logoutStatue">
-                                        <li class="ConferenceLine"><a href="">請先登入會員</a></li>
-                                    </ul>
-                                    <ul v-else-if="store.loginStatue">
-                                        <li v-for="item in PushMsgList" :key="item.mssid"
-                                            :style="{ 'border-left-color': item.color }">
-                                            <a href="">
-                                                {{ item.SendTime }} {{ item.TitleName }} {{ item.SubTitle
-                                                }}
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="AnnTop">
-                                    <div class="AnnTopIcon"><img src="../assets/img/news_icon.svg" alt=""></div>
-                                    <div class="AnnTopText">最新動態</div>
-                                </div>
-                                <div class="AnnContent">
-                                    <ul v-if="store.logoutStatue">
-                                        <li class="ConferenceLine"><a href="">請先登入會員</a></li>
-                                    </ul>
-                                    <ul v-else-if="store.loginStatue">
-                                        <li v-for="item in NewsList" :style="{ 'border-left-color': item.color }">
-                                            <a href="">{{ item.News_Topic }}</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="AnnTop">
-                                    <div class="AnnTopIcon"><img src="../assets/img/gift.svg" alt=""></div>
-                                    <div class="AnnTopText">禮贈推薦</div>
-                                </div>
-                                <div class="AnnContent">
-                                    <ul v-if="store.logoutStatue">
-                                        <li class="ConferenceLine"><a href="">請先登入會員</a></li>
-                                    </ul>
-                                    <ul v-else-if="store.loginStatue">
-                                        <li v-for="item in NewSaleList" :style="{ 'border-left-color': item.color }">
-                                            <a href="">{{ item.News_Topic }}</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </section>
-            </div>
-        </main>
-    </div>
+  <div class="page_main">
+    <main>
+      <div class="page_content">
+        <div class="indexPageTitle">多元服務，價值共享</div>
+        <section class="indexActivityBar">
+          <div class="indAct_item_Box">
+            <ul class="indAct_ul01">
+              <li v-for="item in indexLink" :key="item.id">
+                <router-link :to="`/Course/Meeting/${item.route}`">
+                  <div class="atcImgBox">
+                    <img :src="`/src/assets/img/${item.imgUrl}`" alt="" />
+                  </div>
+                  <div class="atcText" :style="{ color: item.color }">
+                    {{ item.link }}
+                  </div>
+                </router-link>
+              </li>
+            </ul>
+            <ul class="indAct_ul02">
+              <li>
+                <router-link to="#">
+                  <div class="atcImgBox">
+                    <img src="../assets/img/activity_4.svg" alt="" />
+                  </div>
+                  <div class="atcText CaringCare">關懷照護</div>
+                </router-link>
+              </li>
+              <li>
+                <router-link to="#">
+                  <div class="atcImgBox">
+                    <img src="../assets/img/activity_5.svg" alt="" />
+                  </div>
+                  <div class="atcText GiftingPlatform">禮贈平台</div>
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section class="AnnouncementSystem">
+          <div class="AnnouncementSystemBox">
+            <ul>
+              <li>
+                <div class="AnnTop">
+                  <div class="AnnTopIcon">
+                    <img
+                      class="messageIcon"
+                      src="../assets/img/message_icon.svg"
+                      alt=""
+                    />
+                  </div>
+                  <div class="AnnTopText">訊息通知</div>
+                </div>
+                <div class="AnnContent">
+                  <ul v-if="store.logoutStatue">
+                    <li class="ConferenceLine"><a href="">請先登入會員</a></li>
+                  </ul>
+                  <ul v-else-if="store.loginStatue">
+                    <li
+                      v-for="item in PushMsgList"
+                      :key="item.mssid"
+                      :style="{ 'border-left-color': item.color }"
+                    >
+                      <a href="">
+                        {{ item.SendTime }} {{ item.TitleName }}
+                        {{ item.SubTitle }}
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <div class="AnnTop">
+                  <div class="AnnTopIcon">
+                    <img src="../assets/img/news_icon.svg" alt="" />
+                  </div>
+                  <div class="AnnTopText">最新動態</div>
+                </div>
+                <div class="AnnContent">
+                  <ul v-if="store.logoutStatue">
+                    <li class="ConferenceLine"><a href="">請先登入會員</a></li>
+                  </ul>
+                  <ul v-else-if="store.loginStatue">
+                    <li
+                      v-for="(item, index) in NewsList"
+                      :style="{ 'border-left-color': item.color }"
+                      :key="index"
+                    >
+                      <a href="">{{ item.News_Topic }}</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <div class="AnnTop">
+                  <div class="AnnTopIcon">
+                    <img src="../assets/img/gift.svg" alt="" />
+                  </div>
+                  <div class="AnnTopText">禮贈推薦</div>
+                </div>
+                <div class="AnnContent">
+                  <ul v-if="store.logoutStatue">
+                    <li class="ConferenceLine"><a href="">請先登入會員</a></li>
+                  </ul>
+                  <ul v-else-if="store.loginStatue">
+                    <li
+                      v-for="(item, index) in NewSaleList"
+                      :style="{ 'border-left-color': item.color }"
+                      :key="index"
+                    >
+                      <a href="">{{ item.News_Topic }}</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    </main>
+  </div>
 </template>
 
-
-
 <script setup>
-import axios from 'axios';
-import { onMounted, ref } from 'vue';
-import { useLoginStore } from '../stores/stores';
+import axios from "axios";
+import { onMounted, ref } from "vue";
+import { useLoginStore } from "../stores/stores";
 
 const indexLink = [
-    {
-        route: 1,
-        link: '會議活動',
-        imgUrl: 'activity_1.svg',
-        color: '#077E8E'
-    },
-    {
-        route: 2,
-        link: '教育訓練',
-        imgUrl: 'activity_2.svg',
-        color: '#649644'
-    },
-    {
-        route: 3,
-        link: '活動導覽',
-        imgUrl: 'activity_3.svg',
-        color: '#9CAF4A'
-    }
-]
-const store = useLoginStore()
-const PushMsgList = ref([{}]);
-const NewsList = ref([{}]);
-const NewSaleList = ref([{}]);
+  {
+    route: 1,
+    link: "會議活動",
+    imgUrl: "activity_1.svg",
+    color: "#077E8E",
+  },
+  {
+    route: 2,
+    link: "教育訓練",
+    imgUrl: "activity_2.svg",
+    color: "#649644",
+  },
+  {
+    route: 3,
+    link: "活動導覽",
+    imgUrl: "activity_3.svg",
+    color: "#9CAF4A",
+  },
+];
+const store = useLoginStore();
 
 if ($cookies.isKey("AuthCode") == true && $cookies.isKey("u_id") == true) {
-    store.loginStatue = true
-    store.logoutStatue = false
+  store.loginStatue = true;
+  store.logoutStatue = false;
 } else {
-    store.loginStatue = false
-    store.logoutStatue = true
+  store.loginStatue = false;
+  store.logoutStatue = true;
 }
+//個人推播訊息資料
+const PushMsgList = ref([{}]);
+// 最新動態資料清單
+const NewsList = ref([{}]);
+// 禮贈新品資料清單
+const NewSaleList = ref([{}]);
 
 onMounted(() => {
+  //取得個人推播訊息資料
+  const api1 = `${import.meta.env.VITE_APP_API}API_App/HomePage/PushMsgList`;
+  axios
+    .post(api1, {
+      u_id: $cookies.get("u_id"),
+      AuthCode: $cookies.get("AuthCode"),
+      Lang: $cookies.get("Lang"),
+      SDateTime: "",
+      EDateTime: "",
+      Keywords: "",
+    })
+    .then((res) => {
+      PushMsgList.value = res.data.PushMsgDataList;
+    });
 
-    //取得個人推播訊息資料
-    const api1 = `${import.meta.env.VITE_APP_API}API_App/HomePage/PushMsgList`;
-    axios.post(api1, { "u_id": $cookies.get('u_id'), "AuthCode": $cookies.get('AuthCode'), "Lang": $cookies.get('Lang'), "SDateTime": "", "EDateTime": "", "Keywords": "" })
-        .then((res) => {
-            PushMsgList.value = res.data.PushMsgDataList
-        })
-
-    // 最新動態資料清單
-    const api2 = `${import.meta.env.VITE_APP_API}API_App/HomePage/NewsList`;
-    axios.post(api2, { "u_id": $cookies.get('u_id'), "AuthCode": $cookies.get('AuthCode'), "Lang": $cookies.get('Lang') })
-        .then((res) => {
-            NewsList.value = res.data.DataList
-        })
-    // 禮贈新品資料清單
-    const api3 = `${import.meta.env.VITE_APP_API}API_App/HomePage/NewSaleList`;
-    axios.post(api3, { "u_id": $cookies.get('u_id'), "AuthCode": $cookies.get('AuthCode'), "Lang": $cookies.get('Lang') })
-        .then((res) => {
-            NewSaleList.value = res.data.DataList
-        })
-
-
-})
-
-
-
+  // 最新動態資料清單
+  const api2 = `${import.meta.env.VITE_APP_API}API_App/HomePage/NewsList`;
+  axios
+    .post(api2, {
+      u_id: $cookies.get("u_id"),
+      AuthCode: $cookies.get("AuthCode"),
+      Lang: $cookies.get("Lang"),
+    })
+    .then((res) => {
+      NewsList.value = res.data.DataList;
+    });
+  // 禮贈新品資料清單
+  const api3 = `${import.meta.env.VITE_APP_API}API_App/HomePage/NewSaleList`;
+  axios
+    .post(api3, {
+      u_id: $cookies.get("u_id"),
+      AuthCode: $cookies.get("AuthCode"),
+      Lang: $cookies.get("Lang"),
+    })
+    .then((res) => {
+      NewSaleList.value = res.data.DataList;
+    });
+});
 </script>
